ba4f78217dc456e93d5a1ad4b4aa8fdacbbe21c5
Shivansh Randhawa <shivansh.randhawa@gmail.com>
2025-11-09 17:22:10 +1100

    Changed "home" to "notes" to match documentation

--- backend/app/routes/notes.py
+++ backend/app/routes/notes.py
@@ -2,11 +2,11 @@ from flask import Blueprint, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from models import User
 
-home_bp = Blueprint("main", __name__)
+notes_bp = Blueprint("main", __name__)
 
-@home_bp.route("/", methods=["GET"])
+@notes_bp.route("/", methods=["GET"])
 @jwt_required()
-def home():
+def dashboard():
     current_user_email = get_jwt_identity()
     user = User.query.filter_by(email=current_user_email).first()
     return jsonify({
