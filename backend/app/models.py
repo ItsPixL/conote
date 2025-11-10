68e4521319eeee5b9374d204214dfdb58eceeaae
Shivansh Randhawa <shivansh.randhawa@gmail.com>
2025-11-10 11:11:24 +1100

    Progress made on models

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,9 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     firstName = db.Column(db.String(25))
     lastName = db.Column(db.String(25))
@@ -23,3 +25,35 @@ class User(db.Model):
             "lastName": self.lastName,
             "email": self.email
         }
+
+class Note(db.Model):
+    __tablename__ = "notes"
+    id = db.Column(db.Integer, primary_key=True)
+    # user_id = db.Column(db.Integer, db.ForeignKey('users.id')) Will work on establishing relationships soon
+    title = db.Column(db.String(50))
+    createdTime = db.Column(db.DateTime, default=datetime.now())
+    updatedTime = db.Column(db.DateTime, default=datetime.now())
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "createdTime": self.createdTime,
+            "updatedTime": self.updatedTime
+        }
+
+'''
+class Content(db.Model):
+    __tablename__ = "content"
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(), default=None)
+    # image = db.Column() Image storage still in consideration
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "text": self.text,
+            "image": self.image,
+            "updatedTime": self.updatedTime
+        } 
+'''
