3806a4d8895c625cd7f754dceafa031057111a79
Neer Sheth <sheth.neer27@gmail.com>
2025-11-17 14:28:37 +1100

    Made some basic navigation and started on the landing page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import NavBar from "./components/Navbar";
+import Home from "./pages/Home";
 
 export const App = () => {
   return (
     <Router>
       <NavBar />
       <Routes>
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
